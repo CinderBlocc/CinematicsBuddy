af74dcdc92020b03f9970334235326d19243145f
CinderBlocc <cinder.block.rl@gmail.com>
2021-01-22 16:28:15 -0600

    Pulled latest plugin repo for auto build testing. New repo includes bakkesmod props file

--- UpdateScript.bat
+++ UpdateScript.bat
@@ -1,3 +1,19 @@
 ECHO OFF
+
+ECHO.
 ECHO This script currently does nothing. The goal is to write this script to automate the pull/build/copy process for all submodules into the bakkesmod folder
+
+ECHO.
+ECHO Creating CommandVar to store vswhere execution command
+SET CommandVar="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe
+
+ECHO.
+ECHO Running vswhere to store path to MSBuild.exe
+FOR /F "tokens=*" %%F IN ('%CommandVar%') DO SET MSBuildLocation=%%F
+ECHO Found MSBuild.exe: %MSBuildLocation%
+
+ECHO.
+ECHO Rebuilding CinematicsBuddy.sln
+"%MSBuildLocation%" submodules/CinematicsBuddyPlugin/CinematicsBuddy.sln /t:Rebuild
+
 PAUSE
